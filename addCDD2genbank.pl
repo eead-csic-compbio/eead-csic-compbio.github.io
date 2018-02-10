f8220b82108c1cd7a0ceecc53eb16fac0beffc78
vinuesa <vinuesa@ccg.unam.mx>
2018-02-09 19:26:05 -0600

    v1.0; added , , , ,  and db_xref

--- addCDD2genbank.pl
+++ addCDD2genbank.pl
@@ -6,7 +6,7 @@
 # example:
 # ./addCDD2genbank.pl -i RSB205.gbk -o RSB205_pfam.gbk -c /my/oath/to/cdd_delta -d pfam
 
-# 2017-8 Bruno Contreras-Moreira (1) and Pablo Vinuesa (2):
+# 2017 Bruno Contreras-Moreira (1) and Pablo Vinuesa (2):
 # 1: http://www.eead.csic.es/compbio (Laboratory of Computational Biology, EEAD/CSIC/Fundacion ARAID, Spain)
 # 2: http://www.ccg.unam.mx/~vinuesa (Center for Genomic Sciences, UNAM, Mexico)
 
@@ -16,6 +16,12 @@ use strict;
 use Getopt::Std;
 use File::Temp qw/ tempfile /;
 use Bio::SeqIO;
+use File::Basename;
+
+my $progname = basename($0);
+
+our $VERSION = '1.0';   # Feb. 9th, 2018; added strand to Bio::SeqFeature::Generic->new; $seq->add_SeqFeature($misc_feat)
+                        # Feb. 22cnd, 2017; added $progname, $VERSION, $max_target_seqs, $max_hsps, $qcov_hsp_perc and db_xref
 
 my %rank = ( 'source'=>1,'gene'=>2,'CDS'=>3,'misc_feature'=>4 );
 my $CDDERROR = 'need valid -c path to cdd_delta; ftp://ftp.ncbi.nlm.nih.gov/blast/db/cdd_delta.tar.gz';
@@ -25,26 +31,36 @@ my ($MAKEBLASTDB,$DELTABLAST) = ( 'makeblastdb', 'deltablast' );
 my ($INP_threads,$INP_evalue,$INP_CDD,$INP_blastpath,$INP_infile,$INP_outfile,$INP_domaindb) = 
   (2,0.00001,'cdd_delta');
 my ($start,$end,$strand,$pepseq,$domstart,$domend);
-my ($inference,$note,$cddid,$annot,$db,$tag,$value);
+my ($inference,$note,$cddid,$annot,$db,$tag,$value,$db_xref);
 my (%opts,%annotations,@domaindb_names,@trash);
 
-getopts('hc:o:i:p:t:e:d:', \%opts);
+# set defaults
+my $max_target_seqs = 15; # may reduce to 7 or so
+my $max_hsps        = 1;
+my $qcov_hsp_perc   = 97; # may relax, if required
+
+getopts('hc:o:i:p:t:e:d:n:m:q:', \%opts);
 
 if(($opts{'h'})||(scalar(keys(%opts))==0))
 {
-  print "\n[options]: \n";
+  print "\n[options for $progname v.$VERSION]: \n";
   print "-h this message\n";
   print "-i input GenBank file                         (required)\n";
   print "-o output GenBank file with CDD annotations   (required)\n";
   print "-p path to deltablast & makeblastdb           \n";
   print "-c path to CDD formatted for deltablast       \n";
   print "-e max E-value of CDD domain matches          \n";
-  print "-d take only domains from selected databases  (optional, example -d pfam,smart)\n";
+  print "-n max_target_seqs     [def: $max_target_seqs]\n";
+  print "-m max num_HSPs        [def: $max_hsps]       \n";
+  print "-q min qcov_hsp_perc   [def: $qcov_hsp_perc]  \n";
+  print "-d take only domains from selected databases  (optional, example -d pfam,smart,TIGR,cd,COG)\n";
   print "-t number of threads/CPU cores                \n";
   exit(-1);
 }
 
 ## 1) check params
+if(defined($opts{'i'})){ $INP_infile = $opts{'i'} }
+
 if(defined($opts{'i'})){ $INP_infile = $opts{'i'} }
 else{ die "\n# $0 : need -i infile, exit\n"; }
 
@@ -62,6 +78,10 @@ if(defined($opts{'t'}) && $opts{'t'} >= 1){ $INP_threads = int($opts{'t'}) }
 
 if(defined($opts{'e'}) && $opts{'e'} > 0){ $INP_evalue = $opts{'e'} }
 
+if(defined($opts{'m'}) && $opts{'m'} > 0){ $max_hsps = $opts{'m'} }
+
+if(defined($opts{'n'}) && $opts{'n'} > 0){ $max_target_seqs = $opts{'n'} }
+
 if(defined($opts{'d'}))
 { 
   $INP_domaindb = $opts{'d'}; 
@@ -75,7 +95,7 @@ if(defined($opts{'p'}))
   $DELTABLAST  = $INP_blastpath.'/deltablast';
 }
 
-warn "\n# $0 -i $INP_infile -c $INP_CDD -o $INP_outfile -t $INP_threads -e $INP_evalue -d $INP_domaindb -p $INP_blastpath\n";
+warn "\n# $progname v$VERSION -i $INP_infile -c $INP_CDD -o $INP_outfile -t $INP_threads -e $INP_evalue -d $INP_domaindb -p $INP_blastpath -m $max_hsps -n $max_target_seqs -q $qcov_hsp_perc\n";
 
 ## 2) read input GenBank and extract peptide sequences
 my $extracted_peptides = 0;
@@ -106,7 +126,10 @@ close(MAKEDB);
 push(@trash,"$tmpfilename.phr","$tmpfilename.pin","$tmpfilename.psq");
 
 my $annotated_domains = 0;
+
+# added -max_target_seqs 1
 open(DELTA,"$DELTABLAST -num_threads $INP_threads -query $tmpfilename -db $tmpfilename ".
+  "-max_target_seqs $max_target_seqs -max_hsps $max_hsps -qcov_hsp_perc $qcov_hsp_perc ".
   "-rpsdb $INP_CDD -evalue $INP_evalue -show_domain_hits -outfmt \"6 std qlen stitle\" |") ||
   die "# $0 : need a valid \$DELTABLAST path (please use -p path)\n";
 while(my $line = <DELTA>)
@@ -144,7 +167,8 @@ while(my $line = <DELTA>)
   }
 
   # format annotation for GenBank and save
-  $inference = "protein motif:CDD:$cddid";
+  $inference = "protein motif:$cddid";
+  $db_xref = "$cddid";   # << PV added
   $note = $data[13];
   push( @{$annotations{$strand}{$start}{$end}}, [ $domstart,$domend,$inference, $note ] );
   $annotated_domains++;
@@ -197,7 +221,8 @@ while( my $seq = $ingbk->next_seq())
           -tag => { 
             $tag      => $value,
             inference => $inference,
-            note      => $note
+            note      => $note,
+	    db_xref   => $db_xref
         });
 
         $seq->add_SeqFeature($misc_feat);
