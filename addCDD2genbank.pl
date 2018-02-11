312a5571ee592d3d2a1a488779b64b60f82d2c6b
vinuesa <vinuesa@ccg.unam.mx>
2018-02-10 23:42:11 -0600

    excluded , to make insdc-compliant; added function and extended note

--- addCDD2genbank.pl
+++ addCDD2genbank.pl
@@ -4,7 +4,7 @@
 # contained in CDS features within input GenBank file 
 
 # example:
-# ./addCDD2genbank.pl -i RSB205.gbk -o RSB205_pfam.gbk -c /my/oath/to/cdd_delta -d pfam
+# ./addCDD2genbank.pl -i RSB205.gbk -o RSB205_pfam.gbk -c /my/path/to/cdd_delta -d pfam
 
 # 2017 Bruno Contreras-Moreira (1) and Pablo Vinuesa (2):
 # 1: http://www.eead.csic.es/compbio (Laboratory of Computational Biology, EEAD/CSIC/Fundacion ARAID, Spain)
@@ -20,7 +20,10 @@ use File::Basename;
 
 my $progname = basename($0);
 
-our $VERSION = '1.0';   # Feb. 9th, 2018; added strand to Bio::SeqFeature::Generic->new; $seq->add_SeqFeature($misc_feat)
+our $VERSION = '1.1';   # Feb 10th, 2018; excluded $inference, to make insdc-compliant; see http://www.insdc.org/documents/feature_table.html#5; Feature Key misc_feature; see S_malt_D457 genbank
+                        #   increased default qcov_hsp_perc = 100; fixed db_xref (added to annonymous array); 
+			#   changed:  note = "Region similar to domain $domain; length:$length, gap_open:$gap_open, evalue:$evalue";
+                        # Feb. 9th, 2018; added strand to Bio::SeqFeature::Generic->new; $seq->add_SeqFeature($misc_feat)
                         # Feb. 22cnd, 2017; added $progname, $VERSION, $max_target_seqs, $max_hsps, $qcov_hsp_perc and db_xref
 
 my %rank = ( 'source'=>1,'gene'=>2,'CDS'=>3,'misc_feature'=>4 );
@@ -28,16 +31,15 @@ my $CDDERROR = 'need valid -c path to cdd_delta; ftp://ftp.ncbi.nlm.nih.gov/blas
 my $BLASTERROR = 'need valid -p path to deltablast; ftp://ftp.ncbi.nlm.nih.gov/blast/executables/LATEST/';
 
 my ($MAKEBLASTDB,$DELTABLAST) = ( 'makeblastdb', 'deltablast' );
-my ($INP_threads,$INP_evalue,$INP_CDD,$INP_blastpath,$INP_infile,$INP_outfile,$INP_domaindb) = 
-  (2,0.00001,'cdd_delta');
+my ($INP_CDD,$INP_threads,$INP_evalue,$INP_blastpath,$INP_infile,$INP_outfile,$INP_domaindb) = ('cdd_delta',2);
 my ($start,$end,$strand,$pepseq,$domstart,$domend);
-my ($inference,$note,$cddid,$annot,$db,$tag,$value,$db_xref);
+my ($inference,$note,$cddid,$domain,$function,$length,$gap_open,$evalue,$annot,$db,$tag,$value,$db_xref);
 my (%opts,%annotations,@domaindb_names,@trash);
 
 # set defaults
 my $max_target_seqs = 15; # may reduce to 7 or so
 my $max_hsps        = 1;
-my $qcov_hsp_perc   = 97; # may relax, if required
+my $qcov_hsp_perc   = 100; # may relax, if required
 
 getopts('hc:o:i:p:t:e:d:n:m:q:', \%opts);
 
@@ -49,18 +51,16 @@ if(($opts{'h'})||(scalar(keys(%opts))==0))
   print "-o output GenBank file with CDD annotations   (required)\n";
   print "-p path to deltablast & makeblastdb           \n";
   print "-c path to CDD formatted for deltablast       \n";
-  print "-e max E-value of CDD domain matches          \n";
-  print "-n max_target_seqs     [def: $max_target_seqs]\n";
-  print "-m max num_HSPs        [def: $max_hsps]       \n";
-  print "-q min qcov_hsp_perc   [def: $qcov_hsp_perc]  \n";
-  print "-d take only domains from selected databases  (optional, example -d pfam,smart,TIGR,cd,COG)\n";
-  print "-t number of threads/CPU cores                \n";
+  print "-e max E-value of CDD domain matches      [def: $INP_evalue]\n";
+  print "-n max_target_seqs                        [def: $max_target_seqs]\n";
+  print "-m max num_HSPs                           [def: $max_hsps]       \n";
+  print "-q min qcov_hsp_perc                      [def: $qcov_hsp_perc]  \n";
+  print "-d take only domains from selected databases  (optional, example -d pfam,smart,TIGR,cd,COG,PRK)\n";
+  print "-t number of threads/CPU cores            [def: $INP_threads]\n";
   exit(-1);
 }
 
 ## 1) check params
-if(defined($opts{'i'})){ $INP_infile = $opts{'i'} }
-
 if(defined($opts{'i'})){ $INP_infile = $opts{'i'} }
 else{ die "\n# $0 : need -i infile, exit\n"; }
 
@@ -75,12 +75,18 @@ if(defined($opts{'c'}))
 else{ die "# $0 : $CDDERROR\n" }
 
 if(defined($opts{'t'}) && $opts{'t'} >= 1){ $INP_threads = int($opts{'t'}) }
+else{ $INP_threads = 2 }
+
 
 if(defined($opts{'e'}) && $opts{'e'} > 0){ $INP_evalue = $opts{'e'} }
+else { $INP_evalue = 0.001 }
 
 if(defined($opts{'m'}) && $opts{'m'} > 0){ $max_hsps = $opts{'m'} }
 
 if(defined($opts{'n'}) && $opts{'n'} > 0){ $max_target_seqs = $opts{'n'} }
+
+if(defined($opts{'q'}) && $opts{'q'} > 0){ $qcov_hsp_perc = $opts{'q'} }
+
 
 if(defined($opts{'d'}))
 { 
@@ -95,7 +101,7 @@ if(defined($opts{'p'}))
   $DELTABLAST  = $INP_blastpath.'/deltablast';
 }
 
-warn "\n# $progname v$VERSION -i $INP_infile -c $INP_CDD -o $INP_outfile -t $INP_threads -e $INP_evalue -d $INP_domaindb -p $INP_blastpath -m $max_hsps -n $max_target_seqs -q $qcov_hsp_perc\n";
+print "\n# $progname v$VERSION -i $INP_infile -c $INP_CDD -o $INP_outfile -t $INP_threads -e $INP_evalue -d $INP_domaindb -p $INP_blastpath -m $max_hsps -n $max_target_seqs -q $qcov_hsp_perc\n";
 
 ## 2) read input GenBank and extract peptide sequences
 my $extracted_peptides = 0;
@@ -128,13 +134,23 @@ push(@trash,"$tmpfilename.phr","$tmpfilename.pin","$tmpfilename.psq");
 my $annotated_domains = 0;
 
 # added -max_target_seqs 1
+
+my $cmd;
+$cmd = "$DELTABLAST -num_threads $INP_threads -query $tmpfilename -db $tmpfilename ".
+  "-max_target_seqs $max_target_seqs -max_hsps $max_hsps -qcov_hsp_perc $qcov_hsp_perc ".
+  "-rpsdb $INP_CDD -evalue $INP_evalue -show_domain_hits -outfmt \"6 qaccver saccver pident length mismatch gapopen qstart qend sstart send evalue bitscore qlen stitle qcovhsp\"";
+  
+print "$cmd\n";
+
 open(DELTA,"$DELTABLAST -num_threads $INP_threads -query $tmpfilename -db $tmpfilename ".
   "-max_target_seqs $max_target_seqs -max_hsps $max_hsps -qcov_hsp_perc $qcov_hsp_perc ".
-  "-rpsdb $INP_CDD -evalue $INP_evalue -show_domain_hits -outfmt \"6 std qlen stitle\" |") ||
+  "-rpsdb $INP_CDD -evalue $INP_evalue -show_domain_hits -outfmt \"6 qaccver saccver pident length mismatch gapopen qstart qend sstart send evalue bitscore qlen stitle qcovhsp\" |") ||
   die "# $0 : need a valid \$DELTABLAST path (please use -p path)\n";
+  
+   
 while(my $line = <DELTA>)
 {
-  #qaccver saccver pident length mismatch gapopen qstart qend sstart send evalue bitscore qlen stitle
+  #qaccver saccver pident length mismatch gapopen qstart qend sstart send evalue bitscore qlen stitle qcovhsp
   #1:46199:46990 gnl|CDD|214794  28.57 112 63  5 10  118 1 98  1e-04 40.0  263 smart00731, SprT...
   #-1:32308:35544 gnl|CDD|279750  38.00 50  27  1 854 899 178 227 0.001 40.8  1078  pfam01443, Viral...
   chomp;
@@ -153,7 +169,15 @@ while(my $line = <DELTA>)
 
   # compute domain boundaries in the genome
   ($strand,$start,$end) = split(/:/,$data[0],3); 
-  $cddid = (split(/\|/,$data[1],3))[-1];
+  $db_xref = (split(/\|/,$data[1],3))[-1];
+  $domain = (split(/,/,$data[13],2))[0];
+  $length = $data[3];
+  $gap_open = $data[5];
+  $evalue = $data[10];
+  $note = "Region similar to domain $domain; length:$length, gap_open:$gap_open, evalue:$evalue";
+  $function = (split(/,/,$data[13],2))[1];
+  $function =~ s/^\h+//;
+  chomp($function);
 
   if($strand == 1)
   {
@@ -167,10 +191,11 @@ while(my $line = <DELTA>)
   }
 
   # format annotation for GenBank and save
-  $inference = "protein motif:$cddid";
-  $db_xref = "$cddid";   # << PV added
-  $note = $data[13];
-  push( @{$annotations{$strand}{$start}{$end}}, [ $domstart,$domend,$inference, $note ] );
+  #$inference = "protein motif:$cddid";
+  #$db_xref = $cddid;   # << PV added
+  #$note = $data[13];
+  #push( @{$annotations{$strand}{$start}{$end}}, [ $domstart,$domend,$inference,$db_xref,$note ] );
+  push( @{$annotations{$strand}{$start}{$end}}, [ $domstart,$domend,$db_xref,$note,$function ] );
   $annotated_domains++;
 }
 close(DELTA);
@@ -211,7 +236,8 @@ while( my $seq = $ingbk->next_seq())
 
       foreach $annot (@{$annotations{$strand}{$start}{$end}})
       {
-        ($domstart,$domend,$inference,$note) = (@$annot);
+        #($domstart,$domend,$inference,$db_xref,$note) = (@$annot);
+	($domstart,$domend,$db_xref,$note,$function) = (@$annot);
 
         my $misc_feat = Bio::SeqFeature::Generic->new(
           -start =>$domstart,
@@ -220,7 +246,8 @@ while( my $seq = $ingbk->next_seq())
           -primary_tag => 'misc_feature',
           -tag => { 
             $tag      => $value,
-            inference => $inference,
+            #inference => $inference,
+	    function => $function,
             note      => $note,
 	    db_xref   => $db_xref
         });
