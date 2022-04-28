67239af9a3a05ad2b4b05e787d5fddff20222e99
eead-csic-compbio <bcontreras@eead.csic.es>
2022-04-28 16:15:46 +0200

    updated URL in header

--- sitemap.pl
+++ sitemap.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl
 # based on https://gist.github.com/pdp7/9784888
 #
-# https://www.google.com/ping?sitemap=https://www.eead.csic.es/compbio/sitemap.xml
+# https://www.google.com/ping?sitemap=https://eead-csic-compbio.github.io/sitemap.xml
 
 use strict;
 use warnings;
